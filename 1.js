eeafa60f34d1ea00c6c9e675d4afddd199d543c2
Lewis Booth <lewis@amp.studio>
2022-12-08 14:31:22 +0000

    Cleanup and day 2

--- 1.js
+++ 1.js
@@ -1,25 +1,16 @@
-const fs = require('fs')
-const input = fs.readFileSync('input/1', 'UTF-8').split('\n')
+const input = require('fs').readFileSync('input/1', 'UTF-8')
 
-const elves = []
-let currentTotal = 0
-
-// Break input into individual totals per elf
-input.forEach(line => {
-    const num = parseInt(line)
-    if (!num) {
-        elves.push(currentTotal)
-        currentTotal = 0
-    } else {
-        currentTotal += num
-    }
-})
-
-const sortedElves = elves.sort((a, b) => a - b)
+// Break input into sorted totals per elf
+const elves = input
+  .split('\n\n')
+  .map(elf => elf
+    .split('\n')
+    .map(line => parseInt(line))
+    .reduce((a, b) => a + b))
+  .sort((a, b) => a - b)
 
 // The maximum number of calories that any elf is carrying
-console.log('Stage 1: ' + sortedElves.slice(-1))    
+console.log('Stage 1: ' + elves.slice(-1))    
 
 // The sum of calories carried by the top 3 elves
-console.log('Stage 2: ' + sortedElves.slice(-3).reduce((a, b) => a + b))
-
+console.log('Stage 2: ' + elves.slice(-3).reduce((a, b) => a + b))
---
+++ 2.js
@@ -0,0 +1,37 @@
+const input = require('fs').readFileSync('input/2', 'UTF-8').split('\n')
+
+let stageOneScore = 0
+let stageTwoScore = 0
+
+// Hard-code the outcomes
+const stageOne = {
+  "A X": 4,
+  "A Y": 8,
+  "A Z": 3,
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+  "C X": 7,
+  "C Y": 2,
+  "C Z": 6,
+}
+
+const stageTwo = {
+  "A X": 3,
+  "A Y": 4,
+  "A Z": 8,
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+  "C X": 2,
+  "C Y": 6,
+  "C Z": 7,
+}
+
+input.forEach(round => {
+  stageOneScore += stageOne[round]
+  stageTwoScore += stageTwo[round]
+})
+
+console.log('Stage 1: ' + stageOneScore)    
+console.log('Stage 2: ' + stageTwoScore)    
